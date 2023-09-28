cb570c1a09bed09a59ccaced2afb3c37aafcd345
Fakari31 <achmadmusyawir12345@gmail.com>
2023-09-28 15:36:43 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:apk/splashscreen.dart';
 import 'package:flutter/material.dart';
-import 'package:apk/home.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:apk/splashscreen.dart';
 import 'package:apk/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
